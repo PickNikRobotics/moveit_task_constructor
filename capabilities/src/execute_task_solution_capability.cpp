3f4c599f71ce17d266efdd7f6bbb01b3ae0c7847
Abishalini <abi.gpuram@gmail.com>
2023-10-17 17:00:07 -0600

    Remove commented code

--- capabilities/src/execute_task_solution_capability.cpp
+++ capabilities/src/execute_task_solution_capability.cpp
@@ -180,7 +180,6 @@ bool ExecuteTaskSolutionCapability::constructMotionPlan(const moveit_task_constr
 			for (auto& scene_diff : scene_diffs) {
 				if (!moveit::core::isEmpty(scene_diff)) {
 					/* RCLCPP_DEBUG_STREAM(LOGGER, "apply effect of " << description); */
-					// moveit_msgs::msg::PlanningScene scene_diff = const_scene_diff;
 					scene_diff.robot_state.joint_state = sensor_msgs::msg::JointState();
 					scene_diff.robot_state.multi_dof_joint_state = sensor_msgs::msg::MultiDOFJointState();
 					if (!context_->planning_scene_monitor_->newPlanningSceneMessage(scene_diff))
